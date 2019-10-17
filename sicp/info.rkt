4c6ffc691fd5e209df267b39c61f32115b9603df
Maksim Kiryanov <mxvkiryanov@gmail.com>
2019-10-17 18:54:04 +0300

    Init sicp repository
    * Add Makefile with test target
    * Add tmux startup script for sicp project
    * Ignore VIM specific files

---
+++ sicp/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+
+(define name "sicp-racket")
+
+(define homepage "https://github.com/mxv2/books/tree/sicp/sicp")
+
---
+++ sicp/start-tmux.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+tmux new-session \; \
+  split-window -v -p 20 \; \
+  split-window -h -p 50 -t 0 \; \
+  select-pane -t 1 \; \
+  send-keys 'racket -i -l xrepl sicp' C-m \; \
+  select-pane -t 2 \; \
+  send-keys 'git fetch && git status' C-m \; \
+  select-pane -t 2 \; \
+  send-keys 'make test' C-m \; \
+  select-pane -t 0 \; \
+  attach
+
