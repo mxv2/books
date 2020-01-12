2be01f7fa6d6cc3e567d3f9506dfac4c457f7c42
Maksim Kiryanov <mxvkiryanov@gmail.com>
2020-01-12 18:34:08 +0300

    Make ex. 1.23

---
+++ sicp/sicp-racket/chapter1/23.rkt
@@ -0,0 +1,6 @@
+; Improve smallest_divisor algorithm by introducing
+; procedure 'next' for test-divisor.
+;
+; Run time decreases down to 2 for big numbers.
+; For small numbers time practically did not decrease.
+
--- sicp/sicp-racket/chapter1/smallest_divisor.rkt
+++ sicp/sicp-racket/chapter1/smallest_divisor.rkt
@@ -6,11 +6,17 @@
          n)
         ((divides? test-divisor n) 
          test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor)))))
 
 (define (square x)
   (* x x))
 
 (define (divides? a b)
     (= (remainder b a) 0))
+
+;; Improvement from exercise 1.23
+(define (next test-divisor)
+  (if (= test-divisor 2)
+    3
+    (+ test-divisor 2)))
 
